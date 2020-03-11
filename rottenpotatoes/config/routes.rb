c1dc4b9bbb1cc285e90bad87a845fe806cbe3b6e
Arif Arman <arif.arman046@gmail.com>
2020-03-11 03:45:40 +0000

    bug fixes on routes

--- rottenpotatoes/config/routes.rb
+++ rottenpotatoes/config/routes.rb
@@ -2,5 +2,5 @@ Rottenpotatoes::Application.routes.draw do
   resources :movies
   # map '/' to be a redirect to '/movies'
   root :to => redirect('/movies')
-  get 'movies/:id/director_select' => 'movies#director_select', :as => :director_select
+  get 'movies/:id/filter_by_director' => 'movies#filter_by_director', :as => :filter_by_director
 end
